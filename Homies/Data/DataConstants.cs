3c802ec2464e0401ef7b6c47cc96a28ff324ee7f
djanselim <114089140+djanselim@users.noreply.github.com>
2024-03-28 13:19:43 +0200

    Add project files.

---
+++ Homies/Data/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Homies.Data
+{
+	public static class DataConstants
+	{
+		public const int EventNameMaxLength = 5;
+		public const int EventNameMinLength = 20;
+
+		public const int EventDescriptionMaxLength = 150;
+		public const int EventDescriptionMinLength = 15;
+
+		public const string DateFormat = "yyyy-MM-dd H:mm";
+
+		public const int TypeNameMaxLength = 15;
+		public const int TypetNameMinLength = 5;
+	}
+}
